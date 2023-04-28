236303f01f144464e03c98072922268e4d800524
venkatesansubramanian80 <venkatesangit80@outlook.com>
2023-04-28 17:46:29 -0500

    GP-5 End point for Technical Analysis.

--- sentiment_data_puller.py
+++ sentiment_data_puller.py
@@ -23,5 +23,19 @@ def get_sentiment_daa():
         except Exception as e:
             return {'error': str(e)}, 500
 
+@app.route('/api/v1/get_technical_data', methods=['GET'])
+def get_technical_data():
+    if request.method == 'GET':
+        try:
+            influx_connector = influx.InfluxConnector()
+            sql_query = f"SELECT * FROM {os.environ.get('TECHNICAL_ANALYSIS_TABLE')}"
+            print(sql_query)
+            df = influx_connector.read_from_influx(sql_query)
+            if(len(df) == 0):
+                return {'error': 'No Technical Analysis data found'}, 500
+            return df.to_json(orient='records'), 200
+        except Exception as e:
+            return {'error': str(e)}, 500
+
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
