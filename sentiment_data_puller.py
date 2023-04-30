ae18afe1162388fe11972769202fc6518534bd68
venkatesansubramanian80 <venkatesangit80@outlook.com>
2023-04-30 16:39:25 -0500

    GP-5 Making the API as split and merge

--- sentiment_data_puller.py
+++ sentiment_data_puller.py
@@ -11,4 +11,4 @@ app.register_blueprint(technical_analysis)
 app.register_blueprint(fundamental_analysis)
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0', port=5001)
+    app.run(debug=True, host='0.0.0.0')
